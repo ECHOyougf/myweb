9cbb443b6fbd6cb87e62000594e5d453045326cc
echoyougf <youguifang2015>
2016-02-09 18:24:00 +0800

    add todolist by js

---
+++ js/PM/PM.js
@@ -0,0 +1,25 @@
+			//localStorage.tasks=""; //清楚列表
+			if(localStorage.tasks)
+				document.getElementById("list").innerHTML=localStorage.tasks;
+			else localStorage.tasks="";
+			
+			function addtask(){
+				var taskcontent=document.getElementById("taskcontent").value;
+				var taskbox=document.createElement("div");
+				taskbox.className="task";
+				document.getElementById("list").appendChild(taskbox);
+				var inputtext=document.createElement("label");
+				inputtext.innerHTML=taskcontent;
+				taskbox.appendChild(inputtext);
+				
+				var inputcheckbox=document.createElement("input");
+				inputcheckbox.type="checkbox";
+				taskbox.appendChild(inputcheckbox);
+				
+				//将添加的任务存储到localStorage
+				if (window.localStorage) {
+				    localStorage.setItem("tasks",document.getElementById("list").innerHTML);
+					} else {
+    				Cookie.write("tasks", document.getElementById("list").innerHTML);	
+					}
+			}
