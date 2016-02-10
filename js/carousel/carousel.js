81025ff13e60fb701f040f7a0c4c045aac426073
echoyougf <youguifang2015>
2016-02-10 22:05:05 +0800

    updatecrousel

--- js/carousel/carousel.js
+++ js/carousel/carousel.js
@@ -5,20 +5,30 @@ var nextbtn=document.getElementById("next");
 
 function gotopre(){
 	var oBoxs=document.getElementById("box").children;
-	for(var i=1;i<oBoxs.length;i++){
-		if(oBoxs[i].className=="show"){	//获取显示的图片序号i
-				oBoxs[i-1].className="show";//显示前一张
-				oBoxs[i].className="boximg"; //将第i张隐藏
+	if(oBoxs[0].className=="show"){//如果是第一张图片要往回切，切到最后一张吧
+		oBoxs[0].className="boximg";
+		oBoxs[oBoxs.length-1].className="show";
+	}else{
+		for(var i=1;i<oBoxs.length;i++){
+			if(oBoxs[i].className=="show"){	//获取显示的图片序号i
+					oBoxs[i-1].className="show";//显示前一张
+					oBoxs[i].className="boximg"; //将第i张隐藏
+					}
 				}
-			}
+		}
 	}
 
 function gotonext(){
 	var oBoxs=document.getElementById("box").children;
-	for(var i=oBoxs.length-2;i>=0;i--){
-		if(oBoxs[i].className=="show"){	//获取显示的图片序号i
-				oBoxs[i+1].className="show";//显示后一张
-				oBoxs[i].className="boximg"; //将第i张隐藏
-			}
+	if(oBoxs[oBoxs.length-1].className=="show"){//如果是第一张图片要往回切，切到最后一张吧
+		oBoxs[oBoxs.length-1].className="boximg";
+		oBoxs[0].className="show";
+	}else{
+		for(var i=oBoxs.length-2;i>=0;i--){
+			if(oBoxs[i].className=="show"){	//获取显示的图片序号i
+					oBoxs[i+1].className="show";//显示后一张
+					oBoxs[i].className="boximg"; //将第i张隐藏
+				}
+		}
 	}
 }
